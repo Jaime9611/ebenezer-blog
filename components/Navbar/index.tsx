354d704c662b94ec740d2e643263b7cc72ca9e18
Jaime9611 <jaimeescobar9611@gmail.com>
2022-04-27 11:58:26 -0500

    Navbar added dinamic categories

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,18 +1,42 @@
 import Link from 'next/link';
-import React from 'react';
+import { useRouter } from 'next/router';
+import React, { useEffect, useState } from 'react';
 import MenuLink from './MenuLink';
+
+import { Categories as CategoriesList } from '../../types';
+import { getCategories } from '../../services';
 
 type Props = {};
 
 const Navbar = (props: Props) => {
+  const router = useRouter();
+  const [categories, setCategories] = useState<CategoriesList>();
+
+  useEffect(() => {
+    getCategories().then((result) => setCategories(result));
+  }, []);
+
   return (
     <nav className="sticky top-0 z-10 flex h-20 w-full items-center justify-around bg-orange-300">
       <Link href="/">Ebenezer</Link>
-      <ul className="hidden md:flex">
-        <MenuLink text="BLOG" href="/blog" />
-        <MenuLink text="SOBRE MI" href="#sobremi" />
-        <MenuLink text="ESCRIBEME" href="#escribeme" />
-      </ul>
+      {router.pathname === '/blog' ? (
+        <ul className="hidden md:flex">
+          {categories?.map((category) => (
+            <MenuLink
+              key={category.slug}
+              text={category.name.toUpperCase()}
+              href={`/category/${category.slug}`}
+            />
+          ))}
+        </ul>
+      ) : (
+        <ul className="hidden md:flex">
+          <MenuLink text="BLOG" href="/blog" />
+          <MenuLink text="SOBRE MI" href="#sobremi" />
+          <MenuLink text="ESCRIBEME" href="#escribeme" />
+        </ul>
+      )}
+
       <ul className="flex">
         <li className="px-2">
           <a href="#">Facebook</a>
--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -10,7 +10,6 @@ type Props = {
 };
 
 const PostCard = ({ post }: Props) => {
-  console.log(post);
   return (
     <div className="mb-8 rounded-lg bg-white p-0 pb-12 shadow-lg lg:p-8">
       <div className="relative mb-6 overflow-hidden pb-80 shadow-md">
